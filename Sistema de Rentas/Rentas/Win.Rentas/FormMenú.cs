6f346b9fe2aecce37a1fc177bd225c9b9928654d
NahyiMartinez <78700339+NahyiMartinez@users.noreply.github.com>
2021-02-07 22:30:06 -0600

    Agregando proyecto de un Sistema

---
+++ Sistema de Rentas/Rentas/Win.Rentas/FormMenú.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Rentas
+{
+    public partial class FormMenú : Form
+    {
+        public FormMenú()
+        {
+            InitializeComponent();
+        }
+
+        private void reporteDeClíentesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void Login()
+        {
+            var formLogin = new FormLogin();
+            formLogin.ShowDialog();
+        }
+
+        private void productosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formProductos = new FormProductos();
+            formProductos.MdiParent = this;
+            formProductos.Show();
+
+        }
+
+        private void clíentesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formClientes = new FormClientes();
+            formClientes.MdiParent = this;
+            formClientes.Show();
+        }
+
+        private void rentarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formRentas = new FormRentas();
+            formRentas.MdiParent = this;
+            formRentas.Show();
+        }
+
+        private void FormMenú_Load(object sender, EventArgs e)
+        {
+           Login();
+        }
+    }
+}

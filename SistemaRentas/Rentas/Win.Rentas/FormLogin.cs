4c0329cd2a1fa079cafa85e27bdee50b877f0a17
NahyiMartinez <78700339+NahyiMartinez@users.noreply.github.com>
2021-02-08 21:40:25 -0600

    Tienda en línea J&J

---
+++ SistemaRentas/Rentas/Win.Rentas/FormLogin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Rentas
+{
+    public partial class FormLogin : Form
+    {
+        string usuario = "Limny";
+        string contra = "123";
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((tbusuario.Text != usuario || tbcontra.Text != contra))
+            {
+                if (tbusuario.Text != usuario)
+                {
+                    MessageBox.Show("Usuario Incorrecto");
+                    tbusuario.Clear();
+                    tbusuario.Focus();
+                    return;
+                }
+                if (tbcontra.Text != contra)
+                {
+                    MessageBox.Show("Contraseña Incorrecta");
+                    tbcontra.Clear();
+                    tbcontra.Focus();
+                    return;
+                }
+            }
+            else
+            {
+                tbusuario.Clear();
+                tbcontra.Clear();
+                MessageBox.Show("Bienvenido, usuario de J&J Fashion Store");
+                FormMenu Form = new FormMenu();
+                Form.ShowDialog();
+
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ SistemaRentas/Rentas/Win.Rentas/FormMenu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Rentas
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new FormLogin();
+            frm.Show();
+        }
+
+        private void productosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new FormProductos();
+            frm.Show();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new FormClientes();
+            frm.Show();
+        }
+
+        private void rentarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new FormRentar();
+            frm.Show();
+        }
+
+        private void reporteDeProductosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new FormRepProductos();
+            frm.Show();
+        }
+
+        private void reporteDeClientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new FormRepClientes();
+            frm.Show();
+        }
+
+        private void reporteDeRentasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new FormRepRentas();
+            frm.Show();
+        }
+    }
+}
